1cede6da6622974aaf586da808febe240bf95188
shravany856 <115397306+shravany856@users.noreply.github.com>
2023-03-06 19:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+const length = string.length;
+	for(i=0; i<length/2; i++){
+		
+		if(str[i]!==str[length-1-i]){
+			return 'false';
+			
+		}
+	return 'true';
+}
 }
 module.exports = palindrome
